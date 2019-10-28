e30ec914da1076074848a21661aecef3fe3a8c61
Erick Fabricio Martínez Castellanos <mail@erickfabricio.com>
2019-10-28 17:12:57 -0600

    Add token

--- api/middleware/token.js
+++ api/middleware/token.js
@@ -1,19 +1,40 @@
 const jwt = require('jsonwebtoken');
 const config = require('../../config');
 
-options = {
-    //expiresIn: 60 * 60 * 24 //24h
-};
-
-//El payload es un json
 function generateToken(payload) {
-    return jwt.sign(payload, config.key, options);
+    return jwt.sign(
+        {
+            exp: Math.floor(Date.now() / 1000) + (60 * 60),
+            data: payload
+        },
+        config.key
+    );
 }
 
-aplication = {
-    id: "Postman",
-    name: "Test-Dev",
-    state: "A"
+function validateToken(token) {
+    try {
+        let rest = jwt.verify(token, config.key);
+        return rest;
+    } catch (err) {
+        // err
+        return err;
+    }
+
+    /*
+    jwt.verify(token, config.key, function (err, rest, next) {
+        if (err) {
+            return false;
+        }
+        return true;
+    });*/
 }
 
-console.log(generateToken(aplication));
+module.exports = { generateToken, validateToken };
+/*TEST*/
+payload = "erick";
+token = generateToken(payload);
+rest = validateToken(token + "a");
+console.log("payload:" + payload + "\ntoken:" + token + "\nrest:" + rest);
+
+
+
