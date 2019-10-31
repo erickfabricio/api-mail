9dc858ddee30ddc8a9fa728cea076b2bf9200000
Erick Fabricio Martínez Castellanos <mail@erickfabricio.com>
2019-10-31 12:03:01 -0600

    Generación de token

--- api/middleware/token.js
+++ api/middleware/token.js
@@ -1,63 +1,38 @@
 const jwt = require('jsonwebtoken');
 const config = require('../../config');
-const User = require('../models/users');
 
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
-
-async function generate(req, res, next) {
-    console.log(req.body);
-    //Search user        
-    const user = await User.findOne({ mail: req.body.mail });
-    if (user) {
-        bcrypt.compare(req.body.password, user.password, function (err, result) {
-            if (result) {
-                //Generate token
-                let token = jwt.sign(
-                    {
-                        exp: Math.floor(Date.now() / 1000) + (60 * 60 * 24),
-                        data: user.mail
-                    },
-                    config.key
-                );
-                //console.log(token);
-                res.status(200).json({ ok: true, menssage: "Correct login", token: token, user: user });
-            } else {
-                res.status(200).json({ ok: false, menssage: "Incorrect password" });
-            }
-        })
-    } else {
-        res.status(200).json({ ok: false, menssage: "Email not registered" });
-    }
+//retun String
+function generate(payload, seconds) {
+    return jwt.sign(
+        {
+            exp: Math.floor(Date.now() / 1000) + (seconds),
+            data: payload
+        },
+        config.key
+    );
 }
 
-function validate(req, res, next) {
-
-    var token = req.headers['authorization'];
-
-    if (!token) {
-        return res.status(401).send({ ok: false, message: 'Authentication failed' });
-    }
-
-    token = token.replace('Bearer ', '');
-
-    jwt.verify(token, config.key, function (err, data) {
+function validate(token) {
+    return jwt.verify(token, config.key, function (err, info) {
         if (err) {
-            return res.status(401).send({ ok: false, message: 'Token invalid' });
-        } else {
-            console.log(data);
-            //req.token = data
-            next();
+            return { ok: false, err: err };
         }
+        return { ok: true, info: info };
     });
 }
 
 module.exports = { generate, validate };
-/*TEST
-payload = "erick";
-token = generateToken(payload);
-rest = '' //validateToken(token);
-console.log("payload:" + payload + "\ntoken:" + token + "\nrest:" + rest);
-*/
+
+/*TEST*/
+payload = { id: '1234', user: "erick" };
+seconds = 60;
+
+token = generate(payload, seconds);
+resp = validate(token);
+
+console.log("\npayload:" + payload + "\ntoken:" + token + "\nresp:" + JSON.stringify(resp));
+
+
+//console.log(validate('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzI1NDMwODYsImRhdGEiOiJlcmljayIsImlhdCI6MTU3MjU0MzAyNn0.Eh9Y9fb5UcDluHFlfi1_soYgMDOvLGlKtknG1KWiMKk'));
 
 
