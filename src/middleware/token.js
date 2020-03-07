43271ded9a4140509c91770a09c79689b9c6e092
Erick Martínez <mail@erickfabricio.com>
2020-03-07 17:35:24 -0600

    Api mail, protocolos http y https

---
+++ src/middleware/token.js
@@ -0,0 +1,37 @@
+const jwt = require('jsonwebtoken');
+const config = require('../../config');
+
+//retun String
+function generate(payload, seconds) {
+    return jwt.sign(
+        {
+            exp: Math.floor(Date.now() / 1000) + (seconds),
+            data: payload
+        },
+        config.key
+    );
+}
+
+function validate(token) {
+    return jwt.verify(token, config.key, function (err, info) {
+        if (err) {
+            return { ok: false, err: err };
+        }
+        return { ok: true, info: info };
+    });
+}
+
+module.exports = { generate, validate };
+
+/*TEST*/
+payload = { info: "DEV-Postman" };
+seconds = 2592000; //30 dias
+
+token = generate(payload, seconds);
+
+resp = validate(token);
+console.log("\npayload:" + JSON.stringify(payload) + "\n\ntoken:" + token + "\n\nresp:" + JSON.stringify(resp) + "\n");
+
+//console.log(validate('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1NzI1NDMwODYsImRhdGEiOiJlcmljayIsImlhdCI6MTU3MjU0MzAyNn0.Eh9Y9fb5UcDluHFlfi1_soYgMDOvLGlKtknG1KWiMKk'));
+
+
